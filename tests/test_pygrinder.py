92e7293b6af633fad348b8f0da2f4570c5d6c914
Wenjie Du <wenjay.du@gmail.com>
2023-10-18 22:30:15 +0800

    feat: add testing cases for MNAR;

--- tests/test_pygrinder.py
+++ tests/test_pygrinder.py
@@ -9,23 +9,52 @@ import unittest
 
 import numpy as np
 
-from pygrinder import mcar
+from pygrinder import (
+    mcar,
+    mnar_x,
+    mnar_t,
+)
 
 DEFAULT_MISSING_RATE = 0.1
 NAN = 1
 
 
 class TestPyGrinder(unittest.TestCase):
+    complete_time_series = np.random.randn(128, 10, 36)
+
     def test_0_mcar(self):
-        d = np.random.randn(128, 10, 36)
-        d_intact, d_with_missing, missing_mask, indicating_mask = mcar(
-            d, p=DEFAULT_MISSING_RATE, nan=NAN
+        X_intact, X_with_missing, missing_mask, indicating_mask = mcar(
+            self.complete_time_series, p=DEFAULT_MISSING_RATE, nan=NAN
         )
-        shape_product = np.product(d.shape)
+        shape_product = np.product(X_intact.shape)
         actual_missing_rate = (shape_product - missing_mask.sum()) / shape_product
         assert (
             round(actual_missing_rate, 1) == DEFAULT_MISSING_RATE
         ), f"Actual missing rate is {actual_missing_rate}, not given {DEFAULT_MISSING_RATE}"
-        assert np.sum(d_with_missing[(1 - missing_mask).astype(bool)]) == NAN * np.sum(
+        assert np.sum(X_with_missing[(1 - missing_mask).astype(bool)]) == NAN * np.sum(
             1 - missing_mask
         )
+
+    def test_1_mar(self):
+        pass
+
+    def test_2_mnar(self):
+        # mnar_x
+        X_intact, X_with_missing, missing_mask, indicating_mask = mnar_x(
+            self.complete_time_series, offset=0, nan=NAN
+        )
+        shape_product = np.product(X_intact.shape)
+        actual_missing_rate = (shape_product - missing_mask.sum()) / shape_product
+        assert (
+            round(actual_missing_rate, 1) > 0
+        ), f"Actual missing rate is {actual_missing_rate}"
+
+        # mnar_t
+        X_intact, X_with_missing, missing_mask, indicating_mask = mnar_t(
+            self.complete_time_series, cycle=20, pos=10, scale=3, nan=NAN
+        )
+        shape_product = np.product(X_intact.shape)
+        actual_missing_rate = (shape_product - missing_mask.sum()) / shape_product
+        assert (
+            round(actual_missing_rate, 1) > 0
+        ), f"Actual missing rate is {actual_missing_rate}"
