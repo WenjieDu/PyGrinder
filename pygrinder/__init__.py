221992636e8788ce7231c769b878542d4c45c3ed
Wenjie Du <wenjay.du@gmail.com>
2023-11-06 01:07:48 +0800

    feat: releasing v0.2;

--- pygrinder/__init__.py
+++ pygrinder/__init__.py
@@ -21,7 +21,7 @@ PyGrinder: a Python toolkit for grinding data beans into the incomplete.
 #
 # Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
 # 'X.Y.dev0' is the canonical version of 'X.Y.dev'
-__version__ = "0.1.1"
+__version__ = "0.2"
 
 try:
     from pygrinder.missing_completely_at_random import mcar, mcar_little_test
