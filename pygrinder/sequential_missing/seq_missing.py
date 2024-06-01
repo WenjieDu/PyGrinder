9f6f333b8c716d61ef7887710aaad1076cb7a9b7
Wenjie Du <wenjay.du@gmail.com>
2024-06-01 16:26:54 +0800

    refactor: move step shift operation into random_select_start_indices();

--- pygrinder/sequential_missing/seq_missing.py
+++ pygrinder/sequential_missing/seq_missing.py
@@ -5,6 +5,7 @@
 # Created by Wenjie Du <wenjay.du@gmail.com>
 # License: BSD-3-Clause
 
+import math
 from typing import Union
 
 import numpy as np
@@ -12,52 +13,57 @@ import torch
 
 
 def random_select_start_indices(
-    features,
+    feature_idx,
+    step_idx,
     p,
     n_samples,
     n_steps,
     n_features,
-):
-    if features is None:
+) -> np.ndarray:
+    if feature_idx is None:
         all_feature_indices = list(range(n_samples * n_features))
         all_feature_start_indices = [i * n_steps for i in all_feature_indices]
     else:
         all_feature_indices = [
-            i * n_features + j for i in range(n_samples) for j in features
+            i * n_features + j for i in range(n_samples) for j in feature_idx
         ]
         all_feature_start_indices = [i * n_steps for i in all_feature_indices]
 
     selected_feature_start_indices = np.random.choice(
         all_feature_start_indices,
-        round(len(all_feature_start_indices) * p),
+        math.ceil(len(all_feature_start_indices) * p),
         replace=False,
     )
-    return selected_feature_start_indices
+    selected_feature_start_indices = np.asarray(selected_feature_start_indices)
+
+    step_shift = np.random.choice(
+        step_idx,
+        len(selected_feature_start_indices),
+    )
+    step_shift = np.asarray(step_shift)
+
+    selected_start_indices = selected_feature_start_indices + step_shift
+    return selected_start_indices
 
 
 def _seq_missing_numpy(
     X: np.ndarray,
     p: float,
     seq_len: int,
-    features: list = None,
-    steps: list = None,
+    feature_idx: list = None,
+    step_idx: list = None,
 ) -> np.ndarray:
     # clone X to ensure values of X out of this function not being affected
     X = np.copy(X)
 
     n_samples, n_steps, n_features = X.shape
     start_indices = random_select_start_indices(
-        features, p, n_samples, n_steps, n_features
+        feature_idx, step_idx, p, n_samples, n_steps, n_features
     )
 
     X = X.transpose(0, 2, 1)
     X = X.reshape(-1)
     for idx in start_indices:
-        idx += np.random.choice(
-            steps,
-            1,
-            replace=False,
-        )[0]
         X[idx : idx + seq_len] = np.nan
 
     X = X.reshape(n_samples, n_features, n_steps)
@@ -69,25 +75,20 @@ def _seq_missing_torch(
     X: torch.Tensor,
     p: float,
     seq_len: int,
-    features: list = None,
-    steps: list = None,
+    feature_idx: list = None,
+    step_idx: list = None,
 ) -> torch.Tensor:
     # clone X to ensure values of X out of this function not being affected
     X = torch.clone(X)
 
     n_samples, n_steps, n_features = X.shape
     start_indices = random_select_start_indices(
-        features, p, n_samples, n_steps, n_features
+        feature_idx, step_idx, p, n_samples, n_steps, n_features
     )
 
     X = X.transpose(1, 2)
     X = X.flatten()
     for idx in start_indices:
-        idx += np.random.choice(
-            steps,
-            1,
-            replace=False,
-        )[0]
         X[idx : idx + seq_len] = np.nan
 
     X = X.reshape(n_samples, n_features, n_steps)
@@ -99,8 +100,8 @@ def seq_missing(
     X: Union[np.ndarray, torch.Tensor],
     p: float,
     seq_len: int,
-    features: list = None,
-    steps: list = None,
+    feature_idx: list = None,
+    step_idx: list = None,
 ) -> Union[np.ndarray, torch.Tensor]:
     if isinstance(X, list):
         X = np.asarray(X)
@@ -110,44 +111,46 @@ def seq_missing(
     assert isinstance(
         seq_len, int
     ), f"`seq_len` must be type of int, but got {type(seq_len)}"
-    if features is not None:
+    assert seq_len <= n_steps, f"`seq_len` must be <= {n_steps}, but got {seq_len}"
+
+    if feature_idx is not None:
         assert isinstance(
-            features, list
-        ), f"`features` must be type of list, but got {type(features)}"
+            feature_idx, list
+        ), f"`feature_idx` must be type of list, but got {type(feature_idx)}"
 
         assert (
-            max(features) <= n_features
-        ), f"values in `features` must be <= {n_features}, but got {max(features)}"
+            max(feature_idx) <= n_features
+        ), f"values in `feature_idx` must be <= {n_features}, but got {max(feature_idx)}"
 
-    if steps is not None:
+    if step_idx is not None:
         assert isinstance(
-            steps, list
-        ), f"`steps` must be type of list, but got {type(steps)}"
+            step_idx, list
+        ), f"`step_idx` must be type of list, but got {type(step_idx)}"
 
         assert (
-            max(steps) <= n_steps
-        ), f"values in `steps` must be <= {n_steps}, but got {max(steps)}"
+            max(step_idx) <= n_steps
+        ), f"values in `step_idx` must be <= {n_steps}, but got {max(step_idx)}"
         assert (
-            n_steps - max(steps) >= seq_len
-        ), f"n_steps - max(steps) must be >= seq_len, but got {n_steps - max(steps)}"
+            n_steps - max(step_idx) >= seq_len
+        ), f"n_steps - max(step_idx) must be >= seq_len, but got {n_steps - max(step_idx)}"
     else:
-        steps = list(range(n_steps - seq_len))
+        step_idx = list(range(n_steps - seq_len))
 
     if isinstance(X, np.ndarray):
         corrupted_X = _seq_missing_numpy(
             X,
             p,
             seq_len,
-            features,
-            steps,
+            feature_idx,
+            step_idx,
         )
     elif isinstance(X, torch.Tensor):
         corrupted_X = _seq_missing_torch(
             X,
             p,
             seq_len,
-            features,
-            steps,
+            feature_idx,
+            step_idx,
         )
     else:
         raise TypeError(
